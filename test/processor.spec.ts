bc9b4bfd023053b476e0b1e29a8c5165d82064a3
artsevruk <artsevruk@gmail.com>
2024-01-08 15:48:15 +0300

    Add json processor

---
+++ test/processor.spec.ts
@@ -0,0 +1,35 @@
+import {assert} from "chai";
+
+import fs from "fs";
+import path from "path";
+
+import JsonProcessor from "../src/index.js";
+
+const processor = new JsonProcessor("test");
+
+function processAndCompare(filename: string) {
+  const inDoc = fs.readFileSync(path.join('test', 'fixtures', filename), { encoding: 'utf-8' });
+
+  const doc = processor.parse(inDoc);
+  const docStr = JSON.stringify(doc);
+
+  const outDoc = processor.stringify(doc);
+
+  const outDocStructure = processor.parse(outDoc);
+
+  const outDocStructureStr = JSON.stringify(outDocStructure);
+
+  assert.equal(outDocStructureStr, docStr);
+  console.log(filename);
+}
+
+describe('YamlProcessorTest', function() {
+  it('documents should be equal', function() {
+    processAndCompare('simple.json');
+    processAndCompare('matesics.json');
+    processAndCompare('footer.json');
+  });
+});
+
+
+
